31ecdb6477212a5799e50b7fcbe98cd8ab1705fa
aaronbcj <aaronbcj@yahoo.co.in>
2015-09-03 01:14:14 +0530

    2

--- examples/module1/CircularDependencies/b.js
+++ examples/module1/CircularDependencies/b.js
@@ -1,6 +1,6 @@
 console.log('b starting');
 exports.done = false;
 var a = require('./a');
-console.log('in b, a.done = %j', a.done);
+console.log('in b1, a.done = %j', a.done);
 exports.done = true;
 console.log('b done');
